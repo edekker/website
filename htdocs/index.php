ace041a71bbf50027001350454c7b687be469d98
Erica Dekker <edekker@iit.edu>
2010-12-02 17:49:11 -0600

    Created header, nav, and footer includes

---
+++ htdocs/index.php
@@ -0,0 +1,27 @@
+<?php $page_title = "Home: EricaDekker.com"; ?>
+<?php include("includes/header.htm");?>
+
+	<div id="content">
+
+		<div id="main">
+
+			<h2>Salutations</h2>
+
+				<p>I am a full-time student pursuing my master's in technical communication and information design at Illinois Institute of Technology. I have an undergraduate degree in marketing and a training background. While in school, I'm working as a research assistant and freelance writer, editor, and designer, and I have a small consignment business on eBay.</p>
+
+				<p>That's a little bit about me. Feel free to take a look at my resume and portfolio to learn more, and let me know if you have a project that could use my help.</p>
+
+		</div>
+
+	</div>
+
+<?php include("includes/navigation.htm");?>
+
+<?php include("includes/footer.htm");?>
+
+</div>
+
+</body>
+
+</html>
+
